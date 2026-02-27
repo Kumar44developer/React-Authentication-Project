006eef0a4f99af27da3cdbcc2d82a504b1529a88
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-02-27 15:10:12 +0530

    Set isLoggedIn to true for returning user

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -14,5 +14,6 @@ export function AuthProvider({ children }) {
   
     if (storedEmail && storedLoginState === 'true') {
       setUserEmail(storedEmail)
-
+      setIsLoggedIn(true)
+    }
             
