d713ebce173244357b732b5f6e988cd5b8f8dbed
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-02-28 17:37:53 +0530

    created custom useAuth hook to access AuthContext

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -67,3 +67,6 @@ export function AuthProvider({ children }) {
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
 }
+
+export function useAuth() {
+  const context = useContext(AuthContext)
