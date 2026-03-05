54c8e193bc1ca21a572510923594b2f19b74abaa
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-03-05 21:02:29 +0530

    Added fade-in and slide-down animation to error banner

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -79,3 +79,6 @@ function Login() {
           {error && (
             <motion.div
               initial={{ opacity: 0, y: -10 }}
+              animate={{ opacity: 1, y: 0 }}
+              className="error-banner"
+            >
