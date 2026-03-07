00b189593fb0daef6e2473f73098f6f3d3b74125
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-03-07 10:27:20 +0530

    Added transition duration and container class to signup form

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -60,3 +60,6 @@ function Signup() {
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: -20 }}
+      transition={{ duration: 0.3 }}
+      className="signup-container"
+    >
