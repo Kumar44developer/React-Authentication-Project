406f3343d8776ec17bea8b113e91e3eb882cd597
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-03-07 10:19:27 +0530

    Added confirmPassword and errors state in Signup component

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -7,3 +7,5 @@ import './Signup.css'
 function Signup() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [errors, setErrors] = useState({})
