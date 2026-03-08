3a410801315bcdf4fd7baec2ab3cff1f45e746ad
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-03-08 13:00:21 +0530

    Added error styling for password input

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -113,3 +113,5 @@ function Signup() {
                   setErrors({ ...errors, password: '' })
                 }
               }}
+              className={errors.password ? 'input-error' : ''}
+            />
