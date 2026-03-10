dd61e477297d276b5c2476ad8368fc8f4fc2110e
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-03-10 14:24:19 +0530

    Added delayed transition to .welcome-title for smooth entry

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -22,3 +22,6 @@ function Welcome() {
         <motion.h1
           initial={{ opacity: 0, y: -20 }}
           animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.3 }}
+          className="welcome-title"
+        >
