c5aa54f9601814fe0530ed1466391234f4ad76cd
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-03-10 14:23:52 +0530

    Added fade-in and slide-down animation to welcome heading

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -19,3 +19,6 @@ function Welcome() {
         transition={{ delay: 0.2, duration: 0.5 }}
         className="welcome-card"
       >
+        <motion.h1
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
